f84ca083f377bd9dc3b40c6d6df50f58d76ec203
Sojiro Nishimura <sojiro.nishimura@MacBook-Pro-250.local>
2025-10-17 16:21:12 +0900

    Initial commit

---
+++ rss-test/rss_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  rss_testApp.swift
+//  rss-test
+//
+//  Created by Sojiro Nishimura on 2025/10/17.
+//
+
+import SwiftUI
+
+@main
+struct rss_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
